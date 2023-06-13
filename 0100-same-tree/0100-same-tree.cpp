fc67deaba3e9ab32a8d4fea574cbcc40932e2a69
Shobhit Kumar <88700147+iamshobhitkumar@users.noreply.github.com>
2023-06-13 17:42:36 +0530

    Time: 0 ms (100%), Space: 9.9 MB (63.86%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -11,45 +11,13 @@
  */
 class Solution {
 public:
+    bool f(TreeNode* p, TreeNode* q){
+        if(!p && !q) return true;
+        if(!p || !q) return false;
+        if(p->val != q->val) return false;
+        return f(p->left, q->left) && f(p->right, q->right);
+    }
     bool isSameTree(TreeNode* p, TreeNode* q) {
-        if(!p && !q)
-            return true;
-        if(!p || !q)
-            return false;
-        queue<TreeNode*> q1;
-        queue<TreeNode*> q2;
-        q1.push(p);
-        q2.push(q);
-        while(q1.size() && q2.size()){
-            TreeNode* p1 = q1.front();
-            TreeNode* p2 = q2.front();
-            if(p1->val != p2->val)
-                return false;
-            if(p1->right || p1->left){
-                if(p1->right)
-                    q1.push(p1->right);
-                else
-                    q1.push(new TreeNode(1e6));
-                if(p1->left)
-                    q1.push(p1->left);
-                else
-                    q1.push(new TreeNode(1e6));
-            }
-            if(p2->right || p2->left){
-                if(p2->right)
-                    q2.push(p2->right);
-                else
-                    q2.push(new TreeNode(1e6));
-                if(p2->left)
-                    q2.push(p2->left);
-                else
-                    q2.push(new TreeNode(1e6));
-            }
-            q1.pop();
-            q2.pop();
-        }
-        if(q1.size() || q2.size())
-            return false;
-        return true;
+        return f(p,q);
     }
 };
