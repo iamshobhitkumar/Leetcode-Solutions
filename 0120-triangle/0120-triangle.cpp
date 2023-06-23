e9444dbe3c4022a8fe481064763025c67d6b4fc0
Shobhit Kumar <88700147+iamshobhitkumar@users.noreply.github.com>
2023-06-24 02:11:41 +0530

    Time: 4 ms (86.2%), Space: 9.1 MB (7.36%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,25 +1,22 @@
 class Solution {
 public:
-    int f(vector<vector<int>>& triangle, vector<vector<int>>& dp, int i, int j){
-        if(i==0){
-            return triangle[0][0];
-        }
-        if(dp[i][j]!=-1) return dp[i][j];
-        int up = 1e9;
-        cout<<"Up Call for "<<i-1<<" "<<j<<endl;
-        if(j<=i-1) up =  f(triangle, dp, i-1,j);
-        int left = 1e9;
-        cout<<"Left Call for "<<i-1<<" "<<j-1<<endl;
-        if(j-1>=0) left = f(triangle, dp,i-1, j-1);
-        return dp[i][j] = triangle[i][j] + min(up,left);
-    }
     int minimumTotal(vector<vector<int>>& triangle) {
         int n = triangle.size();
-        vector<vector<int>> dp(n,vector<int>(n,-1));
+        vector<vector<int>> dp(n,vector<int>(n,0));
         int mini = 1e9;
+        dp[0][0] = triangle[0][0];
+        for(int i=1;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(j>i)continue;
+                int up = 1e9;
+                if(j<=i-1) up = dp[i-1][j];
+                int left = 1e9;
+                if(j-1>=0) left = dp[i-1][j-1];
+                dp[i][j] = triangle[i][j] + min(up,left);
+            }
+        }
         for(int i=0;i<n;i++){
-            mini = min(mini,f(triangle,dp,n-1,i));
-            cout<<"Loop for "<<i<<endl;
+            mini = min(mini,dp[n-1][i]);
         }
         return mini;
     }
