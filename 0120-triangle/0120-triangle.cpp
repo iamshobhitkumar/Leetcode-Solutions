4ea56edf39d9dfb12e45c85557b50937df664bd3
Shobhit Kumar <88700147+iamshobhitkumar@users.noreply.github.com>
2023-06-24 02:16:22 +0530

    Time: 3 ms (97.18%), Space: 8.6 MB (87.55%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -2,21 +2,22 @@ class Solution {
 public:
     int minimumTotal(vector<vector<int>>& triangle) {
         int n = triangle.size();
-        vector<vector<int>> dp(n,vector<int>(n,0));
+        vector<int> prev(n,0),curr(n,0);
         int mini = 1e9;
-        dp[0][0] = triangle[0][0];
+        prev[0] = triangle[0][0];
         for(int i=1;i<n;i++){
             for(int j=0;j<n;j++){
                 if(j>i)continue;
                 int up = 1e9;
-                if(j<=i-1) up = dp[i-1][j];
+                if(j<=i-1) up = prev[j];
                 int left = 1e9;
-                if(j-1>=0) left = dp[i-1][j-1];
-                dp[i][j] = triangle[i][j] + min(up,left);
+                if(j-1>=0) left = prev[j-1];
+                curr[j] = triangle[i][j] + min(up,left);
             }
+            prev = curr;
         }
         for(int i=0;i<n;i++){
-            mini = min(mini,dp[n-1][i]);
+            mini = min(mini,prev[i]);
         }
         return mini;
     }
