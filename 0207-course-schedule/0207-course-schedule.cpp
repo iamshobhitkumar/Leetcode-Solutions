e315cacf77fb3edeb9d44dfd48eafefddf707f3d
Shobhit Kumar <88700147+iamshobhitkumar@users.noreply.github.com>
2023-07-13 18:02:30 +0530

    Time: 30 ms (28.81%), Space: 14.6 MB (22.33%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,29 +1,30 @@
 class Solution {
 public:
 
-    bool dfs(vector<vector<int>>& adj, vector<int>& visited, vector<int>& pathVisited, int node){
+    bool dfs(vector<vector<int>>& adj, vector<bool>& visited, vector<bool>& pathVisited, int node){
         visited[node] = true;
         pathVisited[node] = true;
         for(auto it: adj[node]){
             if(!visited[it]){
-                if(!dfs(adj, visited,pathVisited, it)) return false;
+                if(dfs(adj,visited,pathVisited, it))
+                     return true;
             }
-            else{
-                if(pathVisited[it]) return false;
-            }
-            
+            else if(pathVisited[it]) return true;
         }
         pathVisited[node] = false;
-        return true;
+        return false;
     }
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
         vector<vector<int>> adj(numCourses);
         for(auto it: prerequisites){
             adj[it[1]].push_back(it[0]);
         }
-        for(int i=0;i<numCourses; i++){
-            vector<int> visited(numCourses, false), pathVisited(numCourses, false);
-            if(!dfs(adj,visited,pathVisited,i)) return false;
+        vector<bool> visited(numCourses, false), pathVisited(numCourses,false);
+        for(int i=0;i<numCourses;i++){
+            if(!visited[i]){
+                if(dfs(adj, visited, pathVisited, i))
+                    return false;
+            }
         }
         return true;
     }
